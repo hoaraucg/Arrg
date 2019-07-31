14d3809d27bd95c9fffc32d5d1ca486d1c85a073
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-07-31 14:15:45 -0400

    Adding Components before fixing layouts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import "./index.css";
 import DoubleNavigationPage from "./components/navbar/navbar";
 import JumbotronPage from "./components/jumbotron/jumbotron";
+import ProjectsPage from "./components/post/post";
+import FooterPage from "./components/footer/footer";
+import SideNavPage from "./components/sidenav/sidenav";
 
 class App extends Component {
   render() {
     return (
       <MDBContainer>
-        <DoubleNavigationPage />
+        <SideNavPage/>
         <JumbotronPage />
+        <ProjectsPage />
+        <FooterPage />
       </MDBContainer>
     );
   }
