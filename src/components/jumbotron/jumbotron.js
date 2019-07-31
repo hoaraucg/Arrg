6e043193901365a344860b7b53aa469ee3090523
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-07-31 14:10:50 -0400

    updated home screen

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -9,11 +9,10 @@ const JumbotronPage = () => {
           <MDBJumbotron style={{ padding: 0 }}>
             <MDBCol className="text-white text-center py-5 px-4 my-5" style={{ backgroundImage: `url(https://wallpapercave.com/wp/RDGUQX5.jpg)` }}>
               <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Arrg</MDBCardTitle>
-                <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                  optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
-                </p>
-                <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> Walk the Plank....</MDBBtn>
+                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold" id="arrg">Arrg</MDBCardTitle>
+                <h3 className="mx-5 mb-5">Batten down the hatches and<br></br>post your argument to declare a winner.</h3>
+                <br></br>
+                <MDBBtn outline color="white" className="mb-5"> Walk the Plank....</MDBBtn>
               </MDBCol>
             </MDBCol>
           </MDBJumbotron>
