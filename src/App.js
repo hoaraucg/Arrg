8b27a0df87c1278d10bf572a24bb2ff78b0ffcbd
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-08-01 13:22:34 -0400

    Setting up Routes

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch, Nav} from "react-router-dom";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import "./index.css";
+import DoubleNavigationPage from "./components/navbar/navbar";
+import MinimalisticIntro from "./components/jumbotron/index";
+import ProjectsPage from "./components/Post";
+import FooterPage from "./components/footer/footer";
+import SideNavPage from "./components/Sidenav/index";
+import FormPage from "./components/login/login";
+import ModalPage from "./components/modal/modal"
+import SignupPage from "./components/signup/signup"
+import SignUpModal from "./components/signup/signupmodal";
+import LoginModal from "./components/login/modal";
+import RegisterPage from "./components/registration/register";
+
+class App extends Component {
+  render (){
+  return (
+    <Router>
+    <div>
+      <Switch>
+      <Route exact path ="/" component={MinimalisticIntro} /> 
+      <Route exact path ="/home" component={ProjectsPage} /> 
+      <Route exact path="/vote" component={SideNavPage}/>
+      {/* <Route component={NoMatch}/> */}
+      </Switch>
+    </div>
+
+    </Router>
+    
+  );
+}}
+
+export default App;
