c304b0613e4b4721f61c2f56a67ea3590293d57a
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-08-13 11:48:09 -0400

    profile page up

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../utils/Api";
-import { MDBRow, MDBCol, MDBJumbotron, MDBContainer, MDBView, MDBMask, MDBCardBody } from "mdbreact";
+import { MDBRow, MDBCol, MDBCard, MDBJumbotron, MDBContainer, MDBView, MDBMask, MDBCardBody } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -28,13 +28,20 @@ class Profile extends Component {
         const { user } = this.props.auth;
         return (
             <MDBCol size="md-12">
-                <MDBRow size="md-8" className="justify-content-center">
+                <MDBRow size="md-8" className="justify-content-center mt-10">
                     <MDBJumbotron fluid>
                         <MDBContainer>
                             <h2 className="display-4">Hey there {user.name}</h2>
-                            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                         </MDBContainer>
                     </MDBJumbotron>
+                </MDBRow>
+                <MDBRow className="justify-content-center">
+                    <MDBCard size="md-5">
+                        <MDBCardBody>This is some text within a panel body.</MDBCardBody>
+                    </MDBCard>
+                    <MDBCard size="md-5">
+                        <MDBCardBody>This is some text within a panel body.</MDBCardBody>
+                    </MDBCard>
                 </MDBRow>
             </MDBCol>
         )
