a6006088c35dc216349471c9c81bdcdcf271b642
Alexandra Baer <alexbaer@Alexandras-MacBook-Pro.local>
2019-08-13 16:33:20 -0400

    updated votes page

--- client/src/pages/Vote.js
+++ client/src/pages/Vote.js
@@ -10,9 +10,7 @@ import { connect } from "react-redux";
 class Vote extends Component {
   state = {
     argument: [],
-    // totalCount: 0,
-    // sideOneTotal: 0,
-    // sideTwoTotal: 0
+    
   };
 
 
@@ -36,6 +34,7 @@ class Vote extends Component {
       .catch(err => console.log ("this should be the error " + err));
   }
 
+
   handleSideTwo = (id) => {
     const { user } = this.props.auth;
 
@@ -54,6 +53,18 @@ class Vote extends Component {
     })
       .catch(err => console.log ("this should be the error " + err));
   }
+
+  // hideArgument = (id) => {
+  //   API.getArgument(id)
+  //   .then(res=> {
+  //     var data = {
+  //     sideOneVote: res.data.sideOneVote,
+  //     sideTwoVote: res.data.sideTwoVote
+  //     }
+  //     console.log(res.data.sideOneVote.length)
+  //   })
+  //   .catch(err => console.log ("this should be the error " + err));
+  // }
 
   // handleIncrement = (id) => {
   //   var data = {
@@ -97,7 +108,9 @@ class Vote extends Component {
           <MDBCol size="md-12">
 
             <h2>
-              {this.state.argument.map(argue => (
+              {this.state.argument.map(argue => {
+                //ternary here 
+                return ((argue.sideTwoVote.length <= 10) || (argue.sideOneVote.length <= 10)) ?
                 <CardTitle>
                   <h1>
                     <div className="import" key={argue._id}>
@@ -107,7 +120,9 @@ class Vote extends Component {
                           <MDBCol style={{ color: "white", fontSize: 50, border: "1px solid grey", borderOpacity: 0.3 }} size="md-9" ><MDBCardBody className="z-depth-5">{argue.title}</MDBCardBody></MDBCol>
                           <MDBCol style={{ color: "white", fontSize: 18, border: "1px solid grey"}} size="md-1" ><p class="text-center">Votes: <br></br> {argue.totalVotes}</p></MDBCol>
                           <MDBCol style={{ color: "white", fontSize: 30, border: "1px solid grey" }} size="md-10">{argue.main}</MDBCol>
-                          <MDBCol style={{ color: "orange", fontSize: 20 }} size="md-5" id="one" onClick={() => this.handleSideOne(argue._id)}><MDBView hover>
+                          <MDBCol style={{ color: "orange", fontSize: 20 }} size="md-5" id="one" onClick={() => this.handleSideOne(argue._id)}>
+                          {/* {this.hideArgument(argue._id)} */}
+                          <MDBView hover>
                             <img
                               src="http://pngimg.com/uploads/flags/flags_PNG14700.png"
                               className="img-fluid"
@@ -123,6 +138,7 @@ class Vote extends Component {
                           </MDBCol>
                           <MDBCol style={{ color: "orange", fontSize: 20 }} size="md-5" id="two"
                             onClick={() => this.handleSideTwo(argue._id)}>
+                            {/* {this.hideArgument(argue._id)} */}
                             <MDBView hover>
                               <img
                                 src="http://pngimg.com/uploads/flags/flags_PNG14700.png"
@@ -147,7 +163,79 @@ class Vote extends Component {
                     </div>
                   </h1>
                 </CardTitle>
-              ))}
+                :
+
+               
+      
+                <CardTitle>
+                   <MDBView hover>
+                   <img
+                                src="http://pngimg.com/uploads/flags/flags_PNG14700.png"
+                                className="position-center img-fluid"
+                                alt=""
+                                style={{ opacity: 0.3 }}
+
+                              />
+                               <div style={{ position: 'absolute', top: 20, left: 10, right: 10, bottom: 20, justifyContent: 'center', alignItems: 'center' }}></div>
+                    <MDBMask className="flex-center" overlay="green-strong">
+                              <h1 className="position-absolute display-1 white-text" style={{zIndex:1, backgroundColor: "black", opacity: 0.5}}>This Arrg-ument is settled!</h1>
+                      
+  
+                  <h1>
+                    <div className="import" key={argue._id}>
+                      {/* <a href={"/arguments/" + argue._id}> */}
+                      <span>
+                        <MDBRow className="d-flex justify-content-center">
+                          <MDBCol style={{ color: "white", fontSize: 50, border: "1px solid grey", borderOpacity: 0.3 }} size="md-9" ><MDBCardBody className="z-depth-5">{argue.title}</MDBCardBody></MDBCol>
+                          <MDBCol style={{ color: "white", fontSize: 18, border: "1px solid grey"}} size="md-1" ><p class="text-center">Votes: <br></br> {argue.totalVotes}</p></MDBCol>
+                          <MDBCol style={{ color: "white", fontSize: 30, border: "1px solid grey" }} size="md-10">{argue.main}</MDBCol>
+                          <MDBCol style={{ color: "orange", fontSize: 20 }} size="md-5" id="one" onClick={() => this.handleSideOne(argue._id)}>
+                          {/* {this.hideArgument(argue._id)} */}
+                          <MDBView hover>
+                            <img
+                              src="http://pngimg.com/uploads/flags/flags_PNG14700.png"
+                              className="img-fluid"
+                              alt=""
+                              style={{ opacity: 0.3 }}
+
+                            />
+                            <div style={{ position: 'absolute', top: 20, left: 10, right: 10, bottom: 20, justifyContent: 'center', alignItems: 'center' }}><p className="font-weight-bolder">broadSIDE 1:<br></br><br></br>{argue.sideone}</p></div>
+                            {/* <MDBMask className="flex-center" overlay="red-strong">
+                              <p className="white-text">Parley!<br></br>Click to Cast Yer Vote</p>
+                            </MDBMask> */}
+                          </MDBView>
+                          </MDBCol>
+                          <MDBCol style={{ color: "orange", fontSize: 20 }} size="md-5" id="two"
+                            onClick={() => this.handleSideTwo(argue._id)}>
+                            {/* {this.hideArgument(argue._id)} */}
+                            <MDBView hover>
+                              <img
+                                src="http://pngimg.com/uploads/flags/flags_PNG14700.png"
+                                className="img-fluid"
+                                alt=""
+                                style={{ opacity: 0.3 }}
+
+                              />
+                              <div style={{ position: 'absolute', top: 20, left: 10, right: 10, bottom: 20, justifyContent: 'center', alignItems: 'center' }}>
+                                <p className="font-weight-bolder">broadSIDE 2:<br></br><br></br>{argue.sidetwo}</p></div>
+                              {/* <MDBMask className="flex-center" overlay="red-strong">
+                                <p className="white-text font-weight-bolder">Parley!<br></br>Click to Cast Yer Vote</p>
+                              </MDBMask> */}
+                            </MDBView>
+                          </MDBCol>
+                        </MDBRow>
+
+                      </span>
+
+                      {/* </a> */}
+
+                    </div>
+                  </h1>
+                  </MDBMask>
+  </MDBView>
+                </CardTitle>
+                
+              })}
 
             </h2>
 
