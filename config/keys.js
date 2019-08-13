82b2cb0506395a35eb9bcfb7f27faee8b453c85d
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-08-13 10:41:17 -0400

    Changes needed for deploying

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://localhost/reactarrglist" ,
-    secretOrKey: "ilovescotchscotchyscotchscotch"
+    secretOrKey: process.env.SECRET || "ilovescotchscotchyscotchscotch"
   };
